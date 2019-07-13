a80078bceebb5001d87ad738b78a26dafae5bb70
JetDesktop <jethro0610@outlook.com>
2019-07-12 23:06:32 -0700

    Setup prefab hierarchy.

--- Wrench/Assets/Scripts/WrenchController.cs
+++ Wrench/Assets/Scripts/WrenchController.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Wrench : MonoBehaviour
+public class WrenchController : MonoBehaviour
 {
     // Start is called before the first frame update
     void Start()
