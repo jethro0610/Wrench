45d417179a47d38ee5c6c6a04f0e09068967a4b3
JetDesktop <jethro0610@outlook.com>
2019-07-15 01:04:25 -0700

    MovingGate level

---
+++ Wrench/Assets/ScrewBackForth.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ScrewBackForth : MonoBehaviour
+{
+    [SerializeField]
+    ScrewDetector linkedScrew;
+    [SerializeField]
+    float moveSpeed;
+    [SerializeField]
+    Vector2 deltaPosition;
+
+    Vector2 startPosition;
+    float moveTick;
+
+    bool returning= false;
+    // Start is called before the first frame update
+    void Start() {
+        startPosition = transform.position;
+    }
+
+    // Update is called once per frame
+    void Update() {
+        if (linkedScrew.isGrabbed) {
+            if (!returning) {
+                moveTick += moveSpeed * Time.deltaTime;
+            }
+            else {
+                moveTick -= moveSpeed * Time.deltaTime;
+            }
+        }
+        if(moveTick > 1.0f) {
+            returning = true;
+        }
+        if (moveTick < 0.0f) {
+            returning = false;
+        }
+
+        transform.position = Vector2.Lerp(startPosition, startPosition + deltaPosition, moveTick);
+    }
+}
