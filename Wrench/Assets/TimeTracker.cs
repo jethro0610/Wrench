5acef00a35db07b6f2f60626a94186619d3dcb30
JetLaptop <jethro0610@outlook.com>
2019-07-14 22:06:48 -0700

    Add time tracker

---
+++ Wrench/Assets/TimeTracker.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TimeTracker : MonoBehaviour
+{
+    float subtractTime;
+    // Start is called before the first frame update
+    void Start()
+    {
+        DontDestroyOnLoad(this);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void ResetTimer() {
+        subtractTime = Time.time;
+    }
+
+    public float GetPlayTime() {
+        return Time.time - subtractTime;
+    }
+}
